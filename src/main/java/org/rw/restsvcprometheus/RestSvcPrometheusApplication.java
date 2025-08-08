375d8f5c05090660a6f3ce4fe0c9f1d37549703d
rwuniard <rwuniard@hotmail.com>
2025-08-08 16:19:30 -0400

    Initial commit.

---
+++ src/main/java/org/rw/restsvcprometheus/RestSvcPrometheusApplication.java
@@ -0,0 +1,13 @@
+package org.rw.restsvcprometheus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestSvcPrometheusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestSvcPrometheusApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/rw/restsvcprometheus/RestSvcPrometheusApplicationTests.java
@@ -0,0 +1,13 @@
+package org.rw.restsvcprometheus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestSvcPrometheusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
